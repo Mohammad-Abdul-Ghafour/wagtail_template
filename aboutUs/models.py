950297959b9f493876546c3783b814ea9a81570a
Hadeaji <hadeaji@gmail.com>
2022-03-14 16:11:46 +0200

    adding some blocks for about us page

--- aboutUs/models.py
+++ aboutUs/models.py
@@ -9,18 +9,17 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 class AboutUsPage(Page):
     template = "aboutUs/aboutUs.html"
 
-    subtitle = models.CharField(max_length=100, blank=False, null=False)
-
     content = StreamField(
         [
             ("simple_banner", blocks.BannerImageAndTextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+            ("card_blocks", blocks.CardsBlock()),
         ],
         null=True,
         blank=True
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("subtitle"),
         StreamFieldPanel("content"),
     ]
 
@@ -28,3 +27,4 @@ class AboutUsPage(Page):
     class Meta:
         verbose_name = "About Us Page"
         verbose_name_plural = "About Us Pages"
+
--- blocks/blocks.py
+++ blocks/blocks.py
@@ -10,3 +10,36 @@ class BannerImageAndTextBlock(blocks.StructBlock):
         template = "blocks/AboutUsBanner.html"
         icon = "edit"
         label = "Title & Image Banner"
+
+class SimpleRichtextBlock(blocks.RichTextBlock):
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            "link",
+        ]
+
+    class Meta:
+        template = "blocks/richtext_block.html"
+        icon = "edit"
+        label = "Simple Richtext"
+
+class CardsBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True , help_text="Add your title")
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image",ImageChooserBlock(required=True)),
+                ("title",blocks.CharBlock(required=True,max_length=40)),
+                ("text",blocks.TextBlock(required=True)),
+                ("url_button",blocks.URLBlock(required=False))
+            ]
+        )
+    )
+
+    class Meta:
+        template = "blocks/card_block.html"
+        icon="placeholder"
+        label="Cards"
